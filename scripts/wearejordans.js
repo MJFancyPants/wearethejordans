88a18746c8880167cc57a57f26407c02c1ae0995
Myles Jordan <mylesrjordan@gmail.com>
2015-11-15 15:30:29 -0500

    Removed wedding photo carousel. Commented out current fadeIn JQuery functions. Need to alter scrollTop values. Removed Wedding carousel from CSS.

---
+++ scripts/wearejordans.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>350) {
+			$("#nav-bar").fadeOut();
+		} else {
+			$("#nav-bar").fadeIn();
+		}
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>350) {
+			$("#first-date").slideDown();
+			$("#young_text").slideDown();
+		}
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>600) {
+			$("#photo-carousel").fadeIn();
+		}
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>1304) {
+			$("#wedding_text").slideDown();
+			$("#wedding_img").slideDown();
+		}
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>1430) {
+			$("#photo-carousel-wedding").fadeIn();
+		}
+	});	
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>1600) {
+			$("#junior_img").slideDown();
+			$("#junior_text").slideDown();
+		}
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>2081) {
+			$("#photo-carousel-junior").fadeIn();
+		}
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>2260) {
+			$("#future").fadeIn();
+		}
+	});
+
+	
+});
