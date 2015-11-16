60e1a328c4300accde990696d9ef380cac87c68e
Myles Jordan <mylesrjordan@gmail.com>
2015-11-15 20:12:48 -0500

    New Wedding Story animations with JQuery and CSS. Scroll heights corrected. Using CSS visibility hidden.

--- scripts/wearejordans.js
+++ scripts/wearejordans.js
@@ -10,42 +10,42 @@ $(document).ready(function() {
 
 	$(window).scroll(function() {
 		if ($(this).scrollTop()>350) {
-			$("#first-date").slideDown();
-			$("#young_text").slideDown();
+			$("#first-date").css("visibility", "visible").animate({opacity: 1.0}, 500);
+			$("#young_text").css("visibility", "visible").animate({opacity: 1.0}, 500);
 		}
 	});
 
 	$(window).scroll(function() {
 		if ($(this).scrollTop()>600) {
-			$("#photo-carousel").fadeIn();
+			$("#photo-carousel").css('visibility', 'visible').animate({opacity: 1.0}, 500);
 		}
 	});
 
 	$(window).scroll(function() {
-		if ($(this).scrollTop()>1304) {
-			$("#wedding_text").slideDown();
-			$("#wedding_img").slideDown();
+		if ($(this).scrollTop()>1250) {
+			$("#wedding_text").css('visibility', 'visible').animate({opacity: 1.0}, 500);
+			$("#wedding_img").css('visibility', 'visible').animate({opacity: 1.0}, 500);
 		}
 	});
 
-	// $(window).scroll(function() {
-	// 	if ($(this).scrollTop()>1600) {
-	// 		$("#junior_img").slideDown();
-	// 		$("#junior_text").slideDown();
-	// 	}
-	// });
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>1600) {
+			$("#junior_img").css('visibility', 'visible').animate({opacity: 1.0}, 500);
+			$("#junior_text").css('visibility', 'visible').animate({opacity: 1.0}, 500);
+		}
+	});
 
-	// $(window).scroll(function() {
-	// 	if ($(this).scrollTop()>2081) {
-	// 		$("#photo-carousel-junior").fadeIn();
-	// 	}
-	// });
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>2081) {
+			$("#photo-carousel-junior").css('visibility', 'visible').animate({opacity: 1.0}, 500);
+		}
+	});
 
-	// $(window).scroll(function() {
-	// 	if ($(this).scrollTop()>2260) {
-	// 		$("#future").fadeIn();
-	// 	}
-	// });
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>2260) {
+			$("#future").css('visibility', 'visible').animate({opacity: 1.0}, 500);
+		}
+	});
 
 	
 });
